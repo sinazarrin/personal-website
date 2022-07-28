a7aad7c3da90adc7b0c756f5967ec215d470a246
sinazarrin <goldmontain8@gmail.com>
2022-07-28 20:50:56 +0430

    add banner and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './App.css'
+import Banner from './components/Banner'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Banner/>
+    </>
   )
 }
 
