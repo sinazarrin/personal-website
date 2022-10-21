3321620990edf10e7231fbbb31e3519d60d48bdd
sinazarrin <goldmontain8@gmail.com>
2022-10-21 14:53:07 +0330

    add resume download section

--- src/components/MySkills/index.js
+++ src/components/MySkills/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 
+
 const MySkills = () => {
     const [skills, setSkills] = useState([
         { name: 'Html', limit: 90 },
@@ -43,7 +44,9 @@ const MySkills = () => {
 
         </div>
         <div className='flex justify-center sm:flex-row flex-col mt-16'>
-          <a href='./../../cv/سینا زرین2.pdf' className="bg-white text-[#1b1b1b] sm:py-3.5 sm:px-9 py-2 px-6 sm:mb-0 mb-2 uppercase sm:text-[12px] text-[10px] tracking-[3px] sm:mr-10">Download Resume</a>
+            <a href='سینا_زرین.pdf' download>
+              <button className="bg-white text-[#1b1b1b] sm:py-3.5 sm:px-9 py-2 px-6 sm:mb-0 mb-2 uppercase sm:text-[12px] text-[10px] tracking-[3px] sm:mr-10">Download Resume</button>
+            </a>
           <button type="button" className="bg-violet-500 text-white sm:py-3.5 sm:px-9 py-2 px-6 sm:mb-0 mb-2 uppercase sm:text-[12px] text-[10px] tracking-[3px] ">Contact Me</button>
         </div>
       </div>
